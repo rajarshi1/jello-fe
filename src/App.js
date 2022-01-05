ef85686cff0a6bdc11d124df5b61af8d5ee4e4a4
rajarshi1 <ghoshal.rajarshi@gmail.com>
2022-01-05 21:03:52 +0530

    created dev-rajarshi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 // import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 function App() {
+  
+  const loginwithGoogle = ()=>{
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())   
+    .then((userCred)=>{
+        console.log(userCred);
+    })
+  }
+
+  const logout = ()=>{
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log('logged out successfully');
+    }).catch((error) => {
+      // An error happened.
+      console.log(error);
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
        <h1>Jello</h1> 
        <p>An app for all your management needs</p>
+       <button onClick={loginwithGoogle}>Login with Google</button>
+       <button onClick={logout}>Logout</button>
       </header>
     </div>
   );
