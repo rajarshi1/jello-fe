7953831c1dcd34788d101e5c5a24bf1f0e0d4c64
parmodgarg92 <parmodgarg92@gmail.com>
2022-01-06 20:18:31 +0530

    formatting only

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-// import logo from './logo.svg';
-import './App.css';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 
 function App() {
-  
-  const loginwithGoogle = ()=>{
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())   
-    .then((userCred)=>{
+  const loginwithGoogle = () => {
+    firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((userCred) => {
         console.log(userCred);
-    })
-  }
+      });
+  };
 
-  const logout = ()=>{
-    firebase.auth().signOut().then(() => {
-      // Sign-out successful.
-      console.log('logged out successfully');
-    }).catch((error) => {
-      // An error happened.
-      console.log(error);
-    });
-  }
+  const logout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+        console.log("logged out successfully");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
       <BrowserRouter>
--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,16 +1,15 @@
-
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/analytics';
+import firebase from "firebase/compat/app";
+import "firebase/compat/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB3STkr2K055HdMD6kgJKOUQctdCq_hCls",
-    authDomain: "jello-b8362.firebaseapp.com",
-    projectId: "jello-b8362",
-    storageBucket: "jello-b8362.appspot.com",
-    messagingSenderId: "890079533370",
-    appId: "1:890079533370:web:ac9ebc3606ec1bf14c1803",
-    measurementId: "G-M349CN9D0T"
-  };
+  apiKey: "AIzaSyB3STkr2K055HdMD6kgJKOUQctdCq_hCls",
+  authDomain: "jello-b8362.firebaseapp.com",
+  projectId: "jello-b8362",
+  storageBucket: "jello-b8362.appspot.com",
+  messagingSenderId: "890079533370",
+  appId: "1:890079533370:web:ac9ebc3606ec1bf14c1803",
+  measurementId: "G-M349CN9D0T",
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
