c940a53b5257df581ccfc306039f29afdc489aad
parmodgarg92 <parmodgarg92@gmail.com>
2022-01-12 07:38:17 +0530

    remove multiple declaration

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -84,15 +84,6 @@ const Register = () => {
   //   firebase.auth().createUserWithEmailAndPassword(email,password)
   // }
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: '',
-  });
-
-  const { name, email, password, password2 } = formData;
-
   const loginwithEmail = (e)=>{
     e.preventDefault();
   const LoginWithEmail = async (e) => {
