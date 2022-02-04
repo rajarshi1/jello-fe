959703e55db9508ae583aee54b57e5142166f659
rajarshi1 <ghoshal.rajarshi@gmail.com>
2022-02-04 15:59:41 +0530

    solved loader issue

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const { user } = useAuthContext();
+  const { user,authIsReady } = useAuthContext();
+  if(!authIsReady){
+
+    return  <div style={{ alignItems: "center", display: "flex", justifyContent: "center", height: "100vh", width: "100vw" }}>
+                  <CircularProgress />
+                 <span style={{ justifyContent: "center", position: "fixed", top: "55%" }}>Loading...please wait</span>
+            </div>
+  }
+  console.log(user);
   return user ? <Outlet /> : <Navigate to="/login/" />;
 }
 
